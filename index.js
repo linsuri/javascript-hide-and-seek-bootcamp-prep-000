dcfa8c2ffe450090a38cc4d3c54c91aff34318ae
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-05-29 16:36:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,8 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i + 1).toString()
+  }
 }
