3fb236d51427477022e57dbd297df4dde1616afe
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-05-29 16:54:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementById('nested').getElementsByClassName('target');
+  return document.querySelector(getElementById('nested').getElementsByClassName('target');
 }
 
 function increaseRankBy(n) {
