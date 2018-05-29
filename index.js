8d363a0461e8721f6cc84ce2ddc45ee8347c7229
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-05-29 16:50:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementById('nested').getElementsByClassName('target').innerHTML;
+  return document.getElementById('nested').getElementsByClassName('target');
 }
 
 function increaseRankBy(n) {
