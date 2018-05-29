c03c6c0559ba5eaf58b8526ca75400bd7128008d
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-05-29 16:36:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  const lis = document.getElementById('grand-node').querySelectorAll('ul.ranked-list li')
   for (let i = 0; i < lis.length; i++) {
     lis[i].innerHTML = (i + 1).toString()
   }
