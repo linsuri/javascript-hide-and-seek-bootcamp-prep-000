edc952362b7e57996efad325bc0f7ad713cf04ae
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-05-29 17:02:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('div#nested div.target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  const lis = document.querySelectorAll('div#grand-node div');
+  return lis[lis.length - 1];
+}
