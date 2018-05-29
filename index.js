09375edfcadc1316f32a1249dca54ddf4abefe61
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-05-29 16:55:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.querySelector(ul.nested getElementById('nested').getElementsByClassName('target');
+  return document.querySelector(div.nested div#target);
 }
 
 function increaseRankBy(n) {
